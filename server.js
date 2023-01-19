161194703036cc1a8aac0244e4e68f5f8e56ad5d
Danny <93397389+Dcastro99@users.noreply.github.com>
2023-01-18 18:24:40 -0800

    added more env files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 require('dotenv').config();
-
+// -------- O_o --------//
 app.use(cors());
 
 const PORT = process.env.PORT || 3002;
